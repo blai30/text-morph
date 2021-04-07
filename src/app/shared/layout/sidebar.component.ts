e4b4d537d6e259f763af118dade1cfa8ee2bfba8
Brian Lai <brianlai30@outlook.com>
2021-04-06 23:50:54 -0700

    feat: add sidebar mobile menu

--- src/app/shared/layout/sidebar.component.ts
+++ src/app/shared/layout/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout-sidebar',
@@ -6,11 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
+  open = false;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
