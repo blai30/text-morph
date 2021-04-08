b8abc0c9315260c286be1cdf30973149dfb2782b
Brian Lai <brianlai30@outlook.com>
2021-04-08 16:51:50 -0700

    feat: add char limit and case changing

--- src/app/word-case/word-case.component.ts
+++ src/app/word-case/word-case.component.ts
@@ -1,43 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-import { CaseType, TextCase, WordCaseService } from "../services/word-case.service";
+import { TextCase, WordCaseService } from "../services/word-case.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './word-case.component.html'
 })
 export class WordCaseComponent implements OnInit {
-  caseTypes: CaseType[] = [];
   inputText = '';
   outputText = '';
+  textCases: TextCase[] = [];
   selectedCaseType: TextCase = TextCase.Unchanged;
-  trimWhitespace = false;
+  trimWhitespace = true;
   clapItUp = false;
+  charLimit? = null;
 
   constructor(
     private wordCaseService: WordCaseService,
   ) {}
 
   ngOnInit(): void {
-    this.caseTypes = this.wordCaseService.getCaseTypes();
+    this.textCases = this.wordCaseService.getTextCases();
   }
 
   transformText(): void {
-    let output = this.inputText;
+    if (!this.inputText) {
+      return;
+    }
+
+    let text = this.inputText;
+
+    if (this.trimWhitespace) {
+      // Trim trailing whitespace.
+      text = text.trim();
+
+      // Shrink excess spaces between words.
+      let regex = /\s+/g;
+      text = text.replace(regex, ' ');
+    }
 
     switch (this.selectedCaseType) {
       case TextCase.UpperCase:
-        output = output.toUpperCase();
+        text = text.toUpperCase();
         break;
       case TextCase.LowerCase:
-        output = output.toLowerCase();
+        text = text.toLowerCase();
         break;
       case TextCase.SentenceCase:
         break;
       case TextCase.TitleCase:
         break;
       case TextCase.CamelCase:
+        text = this.toCamelCase(text);
         break;
       case TextCase.PascalCase:
+        text = this.toPascalCase(text);
         break;
       case TextCase.SnakeCase:
         break;
@@ -50,6 +66,32 @@ export class WordCaseComponent implements OnInit {
         break;
     }
 
-    this.outputText = output;
+    if (this.clapItUp) {
+      text = text.replace(' ', ' 👏 ');
+    }
+
+    if (this.charLimit != null) {
+      text = text.slice(0, this.charLimit);
+    }
+
+    this.outputText = text;
+  }
+
+  toCamelCase(input: string): string {
+    let word = this.toPascalCase(input);
+    return word.length > 0 ? word.substring(0, 1).toLowerCase() + word.substring(1) : word;
+  }
+
+  toPascalCase(input: string): string {
+    return input.replace(/(?:^|_|-| +)(.)/g, (match, capture) => capture.toUpperCase());
+  }
+
+  toSentenceCase(input: string): string {
+    let word = this.toTitleCase(input);
+    return word.length > 0 ? word.substring(0, 1).toLowerCase() + word.substring(1) : word;
+  }
+
+  toTitleCase(input: string): string {
+    return input.replace(/(?:^|_|-+)(.)/g, (match, capture) => capture.toUpperCase());
   }
 }
