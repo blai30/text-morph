07fc89753035d48a4d9fb4f07057037dbc5552cd
Brian Lai <brianlai30@outlook.com>
2021-04-10 20:49:47 -0700

    refactor: cleanup theme service

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -4,28 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ThemeService {
-  isDark: boolean;
-
   constructor() {
     // On page load or when changing themes, best to add inline in `head` to avoid FOUC
     if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
       document.documentElement.classList.add('dark');
-      this.isDark = true;
     } else {
       document.documentElement.classList.remove('dark');
-      this.isDark = false;
     }
   }
 
   toggleTheme(): void {
-    if (this.isDark) {
+    if (this.isDark()) {
       document.documentElement.classList.remove('dark');
-      this.isDark = false;
+      localStorage.theme = 'light';
     } else {
       document.documentElement.classList.add('dark');
-      this.isDark = true;
+      localStorage.theme = 'dark';
     }
+  }
 
-    localStorage.theme = this.isDark ? 'dark' : 'light';
+  isDark(): boolean {
+    return localStorage.theme === 'dark';
   }
 }
